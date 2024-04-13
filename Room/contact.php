4b806e8f1f1f70e0199a8f5fd4d6ac52f2b5a6e7
Admin <Admin@DESKTOP-00H7G97>
2024-04-13 21:59:51 +0700

    up code contact, header, index

---
+++ Room/contact.php
@@ -0,0 +1,67 @@
+<?php
+ include './header.php';
+?>
+<!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section contact-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text">
+                        <h2>Contact Us</h2>
+                        <div class="breadcrumb-option">
+                            <a href="#"><i class="fa fa-home"></i> Home</a>
+                            <span>Contact</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="contact-map">
+                        <iframe
+                            height="700" style="border:0;" allowfullscreen=""></iframe>
+                        <div class="map-inside">
+                            <i class="icon_pin"></i>
+                            <div class="inside-widget">
+                                <h4>Việt Nam</h4>
+                                <ul>
+                                    <li>Phone: 0964 557 349</li>
+                                    <li>Add: Hutech, Quận 9, TP.HCM, VN</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-lg-7 offset-lg-1">
+                            <div class="contact-text">
+                                <div class="section-title">
+                                    <span>Liên hệ</span>
+                                    <h2>Để lại liên hệ tại đây</h2>
+                                </div>
+                                <form action="#" class="contact-form">
+                                    <input type="text" placeholder="Họ tên">
+                                    <input type="text" placeholder="Email">
+                                    <input type="text" placeholder="Website (nếu có)">
+                                    <textarea placeholder="Lời nhắn"></textarea>
+                                    <button type="submit" class="site-btn">Gửi</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+   
+
